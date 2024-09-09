3563e603baf509446f30c4f2b20b06e12cc599d6
Rohit rathod <rohitrathod6636@gmail.com>
2024-09-09 23:49:07 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def calculator():
+    print("Simple Calculator")
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        result = num1 / num2
+    else:
+        result = "Invalid operator"
+
+    print(f"Result: {result}")
+
+calculator()
